3bff1b0764dfc4090fe6ec69f79e4db1c2e7391e
SPGZ <SPGZ>
2019-01-09 10:16:22 +0800

    修改安装包下载为本地添加

--- oracle-install.sh
+++ oracle-install.sh
@@ -5,35 +5,43 @@ ORA_DEB="oracle-xe_11.2.0-1.0_amd64.deb"
 #
 # download the Oracle installer
 #
-downloadOracle () {
+# downloadOracle () {
 
-	local url="https://github.com/MaksymBilenko/docker-oracle-xe-11g"
+# 	local url="https://github.com/MaksymBilenko/docker-oracle-xe-11g"
 
-	local ora_deb_partial=( 
-		${ORA_DEB}aa 
-		${ORA_DEB}ab 
-		${ORA_DEB}ac
-	)
+# 	local ora_deb_partial=( 
+# 		${ORA_DEB}aa 
+# 		${ORA_DEB}ab 
+# 		${ORA_DEB}ac
+# 	)
+	
+# 	local i=1
+# 	for part in "${ora_deb_partial[@]}"; do     
+# 		echo "[Downloading '$part' (part $i/3)]"
+# 		curl -s --retry 3 -m 60 -o /$part -L $url/blob/master/$part?raw=true
+# 		i=$((i + 1))
 
-	local i=1
-	for part in "${ora_deb_partial[@]}"; do     
-		echo "[Downloading '$part' (part $i/3)]"
-		curl -s --retry 3 -m 60 -o /$part -L $url/blob/master/$part?raw=true
-		i=$((i + 1))
+# 	done
+	
+# 	cat /${ORA_DEB}a* > /${ORA_DEB}
 
-	done
+# 	rm -f /${ORA_DEB}a*
 
-	cat /${ORA_DEB}a* > /${ORA_DEB}
+# }
 
-	rm -f /${ORA_DEB}a*
-
-}
-
-downloadOracle
+# downloadOracle
 
 dpkg --install /${ORA_DEB}
 rm -f /${ORA_DEB}
 
+# 判断文件夹是否存在
+if [ ! -d "/u01/app/oracle/product/11.2.0/xe/config/scripts/" ];then
+echo "/u01/app/oracle/product/11.2.0/xe/config/scripts  not exit."
+echo "/u01/app/oracle/product/11.2.0/xe/config/scripts create..."
+mkdir /u01/app/oracle/product/11.2.0/xe/config/scripts
+else
+echo "/u01/app/oracle/product/11.2.0/xe/config/scripts exit."
+fi
 mv /init.ora       /u01/app/oracle/product/11.2.0/xe/config/scripts
 mv /initXETemp.ora /u01/app/oracle/product/11.2.0/xe/config/scripts
 
